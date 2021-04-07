9e620e4c79d5a154ae761a476ccfa2beec3e3950
Jan Doubek <jan_doubek@hotmail.com>
2021-04-07 13:59:20 +0200

    First version of the AspNetCertTest app

---
+++ Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCertTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var name = HttpContext.User?.Identity?.Name;
+            return $"Test API call successful! Certificate Subject Name: {name}";
+        }
+    }
+}
